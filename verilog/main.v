409168a1e7b6169bbca3d48be0b069739352097a
rboone <rboone125@gmail.com>
2020-06-10 18:13:22 -0700

    finishing corrections to to match fake_main

--- verilog/main.v
+++ verilog/main.v
@@ -34,12 +34,17 @@ module main(
     wire [63:0] omegaOut;
     getOmega omega(CLK67MHZ,sckPort,mosiPort,sselPort,omegaOut);
     
-    wire [3:0] n;
-    msbNumZeros(n,omegaOut[39:24], 1'b1);
+    wire [3:0] n, ntemp;
+	assign n = 4'b1100 - ntemp;
+	
+    // msbNumZeros(n,omegaOut[39:24], 1'b1);
     wire clk;
     clkDivider getClk(CLK67MHZ, n, clk);
     wire [1:0] sdOut;
-    sigma_delta_twopiece sd2(clk,resetPort,omegaOut[39:0],sdOut);
+	wire [39:0] sd_kin;
+	
+	bitShifter bs(CLK67MHZ, omegaOut[39:0], sd_kin, ntemp);
+    sigma_delta_twopiece sd2(clk,resetPort,sd_kin,sdOut);
     reg delayOut1;
     reg delayOut2;
     always @ (posedge CLK67MHZ)
@@ -120,33 +125,3 @@ end
 
 endmodule
 
-module msbNumZeros (
-binary_out , //  4 bit binary output
-encoder_in , //  16-bit input 
-enable       //  Enable for the encoder
-);
-
-output [3:0] binary_out ;
-input  enable ; 
-input [15:0] encoder_in ; 
-
-wire [3:0] binary_out ;
-      
-assign  binary_out  = (!enable) ? 0 : (
-    (encoder_in == 16'bxxxx_xxxx_xxxx_xxx1) ? 0 : 
-    (encoder_in == 16'bxxxx_xxxx_xxxx_xx10) ? 1 : 
-    (encoder_in == 16'bxxxx_xxxx_xxxx_x100) ? 2 : 
-    (encoder_in == 16'bxxxx_xxxx_xxxx_1000) ? 3 : 
-    (encoder_in == 16'bxxxx_xxxx_xxx1_0000) ? 4 : 
-    (encoder_in == 16'bxxxx_xxxx_xx10_0000) ? 5 : 
-    (encoder_in == 16'bxxxx_xxxx_x100_0000) ? 6 : 
-    (encoder_in == 16'bxxxx_xxxx_1000_0000) ? 7 : 
-    (encoder_in == 16'bxxxx_xxx1_0000_0000) ? 8 : 
-    (encoder_in == 16'bxxxx_xx10_0000_0000) ? 9 : 
-    (encoder_in == 16'bxxxx_x100_0000_0000) ? 10 : 
-    (encoder_in == 16'bxxxx_1000_0000_0000) ? 11 : 
-    (encoder_in == 16'bxxx1_0000_0000_0000) ? 12 : 
-    (encoder_in == 16'bxx10_0000_0000_0000) ? 13 : 
-    (encoder_in == 16'bx100_0000_0000_0000) ? 14 : 15); 
-
-endmodule 
